0b1f9e4df1acf6dfc4240dd76ac76902be3ec1e0
amarildotardivo <amarildotardivo@gmail.com>
2024-10-12 00:30:01 -0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App'; // Ajuste o caminho se necessário
+import { act } from 'react'; // Atualização da importação
 
 test('renders learn react link', () => {
-  render(<App />);
+  act(() => {
+    render(<App />);
+  });
+  
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
